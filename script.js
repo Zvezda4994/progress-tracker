39a8514fb94ccdbb8d52555993175e2903dc0826
Aditya Singh <adityaviveksingh@gmail.com>
2025-02-19 12:47:23 -0400

    added images for everest and mariana trench

--- script.js
+++ script.js
@@ -6,22 +6,19 @@ updateProgressBar();
 
 function changeProgress() {
     let inputValue = parseInt(document.getElementById("climb-input").value);
-
     if (isNaN(inputValue)) {
         alert("Enter a valid number!");
         return;
     }
 
     let newProgress = progress + inputValue;
-
-    // Restrict movement within limits (-11000m to 8849m)
     if (newProgress < -11000) newProgress = -11000;
     if (newProgress > 8849) newProgress = 8849;
 
     progress = newProgress;
     localStorage.setItem("progress", progress);
-
     document.getElementById("current-progress").innerText = `Current Progress: ${progress}m`;
+
     updateProgressBar();
 }
 
@@ -30,4 +27,3 @@ function updateProgressBar() {
     const progressHeight = ((progress + 11000) / totalHeight) * 100;
     document.getElementById("progress").style.bottom = `${progressHeight}%`;
 }
-
